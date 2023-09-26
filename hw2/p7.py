b1646ad4d175ee822cb5532cfa6af8eb8a2054f6
David Harrison <harrison@cs.olemiss.edu>
2023-09-25 19:48:58 -0500

    Rename the solution to answer inline with the naming I used elsewhere.

---
+++ hw2/p7.py
@@ -0,0 +1,31 @@
+"""
+**Problem 7**: (2 points) Write a program that compares the performance of the
+`del` operator on lists and dictionaries.  The main program should plot
+the run time of each on the same plot as a function of n.  Also plot
+functions that bound the time complexity and print out what you think
+is the time complexity of `del` operators for lists and dictionaries
+using big-O notation.  When measuring performance on the list `del`
+operator, be sure to delete items at random locations from the list.
+"""
+
+import matplotlib.pyplot as plt
+
+
+def sweep_n(n: int) -> list:
+    execution_secs = []
+
+    lst.
+    for i in range(n):
+    return caps
+
+
+def main():
+
+    plt.scatter(range(1, N), run_time_list[1:], color="blue")
+
+    plt.title('Execution time of an append operation vs. N')
+    plt.xlabel('N')
+    plt.ylabel('t (seconds)')
+    plt.ylim(bottom=0)
+
+    plt.show()
