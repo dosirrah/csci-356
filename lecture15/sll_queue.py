a1f5a8ef2eec7f5ffc7cc481d07c4b87ac98acec
David Harrison <harrison@cs.olemiss.edu>
2023-10-14 01:25:45 -0500

    Implementation of a singly-linked list.

---
+++ lecture15/sll_queue.py
@@ -0,0 +1,25 @@
+"""
+An SSLQueue is a queue based on a singly-linked list.
+"""
+
+from singly_linked_list import SinglyLinkedList
+
+
+class SLLQueue:
+    """Implementation of a Queue based on Python's list."""
+
+    def __init__(self):
+        self._items = SinglyLinkedList()
+
+    def is_empty(self):
+        return len(self._items) == 0
+
+    def enqueue(self, item):
+        # O(n) operation.
+        self._items.push_back(item)
+
+    def dequeue(self):
+        return self._items.pop_front()
+
+    def __len__(self):
+        return len(self._items)
