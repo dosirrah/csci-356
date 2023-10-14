af768781ce425d6637fc6dfe9c4072886507d6bb
David Harrison <harrison@cs.olemiss.edu>
2023-10-14 01:43:32 -0500

    A Python list implementation of a Deque.

---
+++ lecture15and16/list_deque.py
@@ -0,0 +1,26 @@
+"""
+Based on the implementation from 
+"""
+
+
+class Deque:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def addFront(self, item):
+        self.items.append(item)
+
+    def addRear(self, item):
+        self.items.insert(0,item)
+
+    def removeFront(self):
+        return self.items.pop()
+
+    def removeRear(self):
+        return self.items.pop(0)
+
+    def size(self):
+        return len(self.items)
