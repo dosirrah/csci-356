084cf53d7a5e16ae6ee9bce26b59adbc9123db5d
David Harrison <harrison@cs.olemiss.edu>
2023-10-31 01:16:15 -0500

    Trivial changes.

--- lecture18/simple_recurse.py
+++ lecture18/simple_recurse.py
@@ -1,28 +1,29 @@
-import sys
-import matplotlib
+# import sys
+import matplotlib.pyplot as plt
 import time
 
 def f(i):
     if i == 0:
-        #print("reached terminating condition")
+        # print("reached terminating condition")
         return
-    #print(f"f({i})")
-    #sys.stdout.flush()
+    # print(f"f({i})")
+    # sys.stdout.flush()
     f(i-1)
 
 
 
 f(4)
 
-N=60
-M=1000
+N=30
+M=10000
+
 
 def main():
     run_times = []
     for i in range(N):
         start_time = time.time()
-        for j in range(M):
-            f(j)
+        for _ in range(M):
+            f(i)
         end_time = time.time()
         elapsed_time = end_time - start_time
         avg_elapsed_time = elapsed_time / M
