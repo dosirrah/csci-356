b407fd6f3335d219c129d53fe6166476a276ffee
David Harrison <harrison@cs.olemiss.edu>
2023-11-09 10:42:42 -0600

    Fixed linter warnings.  Condense some of the code for quick sort.

--- lecture22/fast_sort_algos.py
+++ lecture22/fast_sort_algos.py
@@ -1,12 +1,9 @@
 import matplotlib.pyplot as plt
-import random
-import sys
-import time
 
 
 def merge_sort(alist):
     # print("Splitting ",alist)
-    if len(alist)>1:
+    if len(alist) > 1:
         mid = len(alist)//2
         lefthalf = alist[:mid]
         righthalf = alist[mid:]
@@ -14,70 +11,56 @@ def merge_sort(alist):
         merge_sort(lefthalf)
         merge_sort(righthalf)
 
-        i=0
-        j=0
-        k=0
+        i = 0
+        j = 0
+        k = 0
         while i < len(lefthalf) and j < len(righthalf):
             if lefthalf[i] <= righthalf[j]:
-                alist[k]=lefthalf[i]
-                i=i+1
+                alist[k] = lefthalf[i]
+                i = i + 1
             else:
-                alist[k]=righthalf[j]
-                j=j+1
-            k=k+1
+                alist[k] = righthalf[j]
+                j = j + 1
+            k = k + 1
 
         while i < len(lefthalf):
-            alist[k]=lefthalf[i]
-            i=i+1
-            k=k+1
+            alist[k] = lefthalf[i]
+            i = i+1
+            k = k+1
 
         while j < len(righthalf):
-            alist[k]=righthalf[j]
-            j=j+1
-            k=k+1
+            alist[k] = righthalf[j]
+            j = j+1
+            k = k+1
     # print("Merging ",alist)
 
 
-def quick_sort(alist):
-    quick_sort_helper(alist, 0, len(alist) - 1)
+def quick_sort(arr):
+    def _sort(first, last):
+        if first < last:
+            splitpoint = partition(arr, first, last)
+            _sort(first, splitpoint - 1)  # left "half"
+            _sort(splitpoint + 1, last)   # right "half"
+
+    _sort(0, len(arr) - 1)
 
 
-def quick_sort_helper(alist, first, last):
-    if first<last:
+def partition(arr, first, last):
+    pivotvalue = arr[first]
+    i = first + 1
+    j = last
+    while True:
+        while i <= j and arr[i] <= pivotvalue:
+            i += 1
+        while arr[j] >= pivotvalue and j >= i:
+            j -= 1
+        if j < i:
+            break
+        else:  # swap
+            arr[i], arr[j] = arr[j], arr[i]
 
-        splitpoint = partition(alist,first,last)
-
-        quick_sort_helper(alist, first, splitpoint - 1)
-        quick_sort_helper(alist, splitpoint + 1, last)
-
-
-def partition(alist,first,last):
-    pivotvalue = alist[first]
-
-    leftmark = first+1
-    rightmark = last
-
-    done = False
-    while not done:
-
-        while leftmark <= rightmark and alist[leftmark] <= pivotvalue:
-            leftmark = leftmark + 1
-
-        while alist[rightmark] >= pivotvalue and rightmark >= leftmark:
-            rightmark = rightmark -1
-
-        if rightmark < leftmark:
-            done = True
-        else:
-            temp = alist[leftmark]
-            alist[leftmark] = alist[rightmark]
-            alist[rightmark] = temp
-
-    temp = alist[first]
-    alist[first] = alist[rightmark]
-    alist[rightmark] = temp
-
-    return rightmark
+    arr[j], arr[first] = arr[first], arr[j]
+    return j
 
 # alist = [54,26,93,17,77,31,44,55,20]
 # quickSort(alist)
@@ -89,17 +72,17 @@ def partition(alist,first,last):
 
 
 if __name__ == "__main__":
-    from lecture21.sort_algos import selection_sort
+    # from lecture21.sort_algos import selection_sort
     from lecture21.sort_algos import insertion_sort
-    from lecture21.sort_algos import perf_test, N, M
+    from lecture21.sort_algos import perf_test, N
 
     def main():
 
         run_times = perf_test(quick_sort)
         plt.scatter(range(1, N + 1), run_times, color="blue")
 
-        #run_times = perf_test(merge_sort)
-        #plt.scatter(range(1, N + 1), run_times, color="purple")
+        # run_times = perf_test(merge_sort)
+        # plt.scatter(range(1, N + 1), run_times, color="purple")
 
         # run_times = perf_test(selection_sort)
         # plt.scatter(range(1, N + 1), run_times, color="red")
